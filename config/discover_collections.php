45174e6b2bdf1a8153d02a097d35a1f403beef28
Kalleu Queiroz <kalleuqueiroz8@gmail.com>
2026-07-07 21:53:25 -0300

    adicionado novos conteudos

--- config/discover_collections.php
+++ config/discover_collections.php
@@ -22,6 +22,7 @@ return [
     */
 
     'collections' => [
+        // --- 1. Destaques Gerais ---
         'em-alta' => [
             'slug' => 'em-alta',
             'title' => 'Em Alta',
@@ -67,6 +68,173 @@ return [
             'type' => 'upcoming',
             'icon' => '📅',
             'params' => [],
+        ],
+
+        // --- 2. Gêneros do Cinema ---
+        'acao' => [
+            'slug' => 'acao',
+            'title' => 'Ação',
+            'subtitle' => 'Adrenalina pura, perseguições e combates eletrizantes',
+            'type' => 'genre',
+            'icon' => '⚔️',
+            'params' => [
+                'genre_id' => 28,
+            ],
+        ],
+
+        'comedia' => [
+            'slug' => 'comedia',
+            'title' => 'Comédia',
+            'subtitle' => 'Dose diária de bom humor, risadas e diversão',
+            'type' => 'genre',
+            'icon' => '😂',
+            'params' => [
+                'genre_id' => 35,
+            ],
+        ],
+
+        'terror' => [
+            'slug' => 'terror',
+            'title' => 'Terror',
+            'subtitle' => 'Sustos, tensão psicológica e mistérios sombrios',
+            'type' => 'genre',
+            'icon' => '👻',
+            'params' => [
+                'genre_id' => 27,
+            ],
+        ],
+
+        'ficcao-cientifica' => [
+            'slug' => 'ficcao-cientifica',
+            'title' => 'Ficção Científica',
+            'subtitle' => 'Viagens espaciais, futuros distópicos e tecnologia avançada',
+            'type' => 'genre',
+            'icon' => '🚀',
+            'params' => [
+                'genre_id' => 878,
+            ],
+        ],
+
+        'romance' => [
+            'slug' => 'romance',
+            'title' => 'Romance',
+            'subtitle' => 'Histórias de amor, encontros marcantes e emoção',
+            'type' => 'genre',
+            'icon' => '❤️',
+            'params' => [
+                'genre_id' => 10749,
+            ],
+        ],
+
+        'drama' => [
+            'slug' => 'drama',
+            'title' => 'Drama',
+            'subtitle' => 'Narrativas profundas sobre a complexidade humana',
+            'type' => 'genre',
+            'icon' => '🎭',
+            'params' => [
+                'genre_id' => 18,
+            ],
+        ],
+
+        'misterio' => [
+            'slug' => 'misterio',
+            'title' => 'Mistério',
+            'subtitle' => 'Casos intrigantes e segredos que exigem decifração',
+            'type' => 'genre',
+            'icon' => '🕵️',
+            'params' => [
+                'genre_id' => 9648,
+            ],
+        ],
+
+        'animacao' => [
+            'slug' => 'animacao',
+            'title' => 'Animação',
+            'subtitle' => 'Arte visual marcante e fantasia para todas as idades',
+            'type' => 'genre',
+            'icon' => '🎨',
+            'params' => [
+                'genre_id' => 16,
+            ],
+        ],
+
+        // --- 3. Filtros Especiais e Descobertas ---
+        'classicos' => [
+            'slug' => 'classicos',
+            'title' => 'Clássicos do Cinema',
+            'subtitle' => 'Obras-primas influentes lançadas até 1980',
+            'type' => 'discover',
+            'icon' => '💎',
+            'params' => [
+                'primary_release_date.lte' => '1980-01-01',
+                'vote_count.gte' => 500,
+                'sort_by' => 'vote_average.desc',
+            ],
+        ],
+
+        'anos-90' => [
+            'slug' => 'anos-90',
+            'title' => 'Clássicos dos Anos 90',
+            'subtitle' => 'A era de ouro das locadoras de VHS e Blockbuster',
+            'type' => 'discover',
+            'icon' => '📼',
+            'params' => [
+                'primary_release_date.gte' => '1990-01-01',
+                'primary_release_date.lte' => '1999-12-31',
+                'sort_by' => 'popularity.desc',
+            ],
+        ],
+
+        'cinema-internacional' => [
+            'slug' => 'cinema-internacional',
+            'title' => 'Cinema Internacional',
+            'subtitle' => 'A riqueza cultural de grandes produções fora do eixo de Hollywood',
+            'type' => 'discover',
+            'icon' => '🌎',
+            'params' => [
+                'without_original_language' => 'en',
+                'sort_by' => 'popularity.desc',
+                'vote_count.gte' => 200,
+            ],
+        ],
+
+        'curtidos-pela-critica' => [
+            'slug' => 'curtidos-pela-critica',
+            'title' => 'Curtidos pela Crítica',
+            'subtitle' => 'Filmes aclamados com média de avaliação superior a 8.0',
+            'type' => 'discover',
+            'icon' => '🏆',
+            'params' => [
+                'vote_average.gte' => 8.0,
+                'vote_count.gte' => 1000,
+                'sort_by' => 'vote_average.desc',
+            ],
+        ],
+
+        'blockbusters' => [
+            'slug' => 'blockbusters',
+            'title' => 'Blockbusters',
+            'subtitle' => 'Os maiores recordistas de bilheteria e arrecadação do cinema',
+            'type' => 'discover',
+            'icon' => '💥',
+            'params' => [
+                'sort_by' => 'revenue.desc',
+            ],
+        ],
+
+        'joias-escondidas' => [
+            'slug' => 'joias-escondidas',
+            'title' => 'Joias Escondidas',
+            'subtitle' => 'Filmes altamente aclamados mas pouco conhecidos do público geral',
+            'type' => 'discover',
+            'icon' => '💎',
+            'params' => [
+                'vote_average.gte' => 7.6,
+                'vote_count.gte' => 100,
+                'vote_count.lte' => 1200,
+                'sort_by' => 'vote_average.desc',
+            ],
         ],
     ],
 ];
